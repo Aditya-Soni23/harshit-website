35338627cbadf8d97294e15fa615c51b015b6c42
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2022-02-23 23:34:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one(){
+    window.alert("BOSS")
+}
+
+function two(){
+    window.alert("THINKING")
+}
+
+function three(){
+    window.alert("BOXING")
+}
+
+function four(){
+    window.alert("POSE")
+}
+
+function five(){
+    window.alert("BALLE BALLE")
+}
+
+function six(){
+    window.alert("SHOOTING")
+}
+
+function seven(){
+    window.alert("SMILE")
+}
